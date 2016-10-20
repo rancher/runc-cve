6cda437855f57d5ea515e007bdc53e3e9dc29cab
Michael Crosby <crosbymichael@gmail.com>
2016-10-20 15:21:52 -0700

    Revert "Set ambient capabilities where supported"
    This reverts commit 4e179bddcaae964084e0afeda36ac68408f39c4b.

--- libcontainer/capabilities_linux.go
+++ libcontainer/capabilities_linux.go
@@ -10,7 +10,7 @@ import (
 	"github.com/syndtr/gocapability/capability"
 )
 
-const allCapabilityTypes = capability.CAPS | capability.BOUNDS | capability.AMBS
+const allCapabilityTypes = capability.CAPS | capability.BOUNDS
 
 var capabilityMap map[string]capability.Cap
 
